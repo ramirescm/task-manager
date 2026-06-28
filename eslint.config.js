59a80a342473796a23ef0cb80ac41930e546a2b2
Ramires <ramirescm@gmail.com>
2026-06-28 17:25:12 -0300

    chore: add prietter and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,10 +14,17 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      configPrettier, // Desativa regras de formatação do ESLint que conflitam com o Prettier
     ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    plugins: {
+      prettier: pluginPrettier, // Adiciona o plugin do Prettier
+    },
+    rules: {
+      'prettier/prettier': 'error', // Reporta erros de formatação como erros do ESLint
+    },
   },
-])
+]);
