51587a794173d0087aac30e397bba95945b27b3e
Ramires <ramirescm@gmail.com>
2026-07-02 20:30:03 -0300

    feat: add TimeSelect component

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -6,7 +6,7 @@ import { CSSTransition } from "react-transition-group"
 
 import Button from "./Button"
 import Input from "./Input"
-import InputLabel from "./InputLabel"
+import TimeSelect from "./TimeSelect"
 
 const AddTaskDialog = ({ isOpen, handleClose }) => {
   const nodeRef = useRef() // used when needed to store a reference to a DOM node, in this case the div that contains the dialog
@@ -35,18 +35,7 @@ const AddTaskDialog = ({ isOpen, handleClose }) => {
               label="Título"
               placeholder="Insira o título da tarefa"
             />
-            <div className="flex flex-col space-y-1 text-left">
-              <InputLabel htmlFor="time">Horário</InputLabel>
-              <select
-                id="time"
-                className="rounded-lg border border-solid border-[#ececec] px-4 py-3 outline-[#00adb5] placeholder:text-sm placeholder:text-[#9a9c9f]"
-              >
-                <option value="">Selecione o período</option>
-                <option value="morning">Manhã</option>
-                <option value="afternoon">Tarde</option>
-                <option value="evening">Noite</option>
-              </select>
-            </div>
+            <TimeSelect />
             <Input
               id="description"
               label="Descrição"
---
+++ src/components/TimeSelect.jsx
@@ -0,0 +1,20 @@
+import InputLabel from "./InputLabel"
+
+const TimeSelect = () => {
+  return (
+    <div className="flex flex-col space-y-1 text-left">
+      <InputLabel htmlFor="time">Horário</InputLabel>
+      <select
+        id="time"
+        className="rounded-lg border border-solid border-[#ececec] px-4 py-3 outline-[#00adb5] placeholder:text-sm placeholder:text-[#9a9c9f]"
+      >
+        <option value="">Selecione o período</option>
+        <option value="morning">Manhã</option>
+        <option value="afternoon">Tarde</option>
+        <option value="evening">Noite</option>
+      </select>
+    </div>
+  )
+}
+
+export default TimeSelect
