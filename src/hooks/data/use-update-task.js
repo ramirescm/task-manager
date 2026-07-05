35c880cc7aac1705b92e1a12b29c354c45719956
Ramires <ramirescm@gmail.com>
2026-07-05 10:42:08 -0300

    refactor: add axios base instance

--- src/hooks/data/use-update-task.js
+++ src/hooks/data/use-update-task.js
@@ -1,15 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import axios from "axios"
+
+import { api } from "../../lib/axios"
 
 export const useUpdateTask = (taskId) => {
   const queryClient = useQueryClient()
   return useMutation({
     mutationKey: ["update-task", taskId],
     mutationFn: async (task) => {
-      const { data: updatedTask } = await axios.patch(
-        `http://localhost:3000/tasks/${taskId}`,
-        task
-      )
+      const { data: updatedTask } = await api.patch(`/tasks/${taskId}`, task)
       return updatedTask
     },
     onSuccess: (updatedTask) => {
